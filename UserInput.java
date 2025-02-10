e6e1b15ab41b17b677396c1e758886f4bf64b233
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-02-10 12:08:08 +0600

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Scanner input1 = new Scanner(System.in);
-
         System.out.print("Enter your name: ");
         String studentName = input.nextLine();
 
