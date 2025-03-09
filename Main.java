afe4066e495e1298989076b008355a22fc3959b8
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-03-10 03:09:45 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
-public class Main {
+class Pen {
+    String name;
+    String color;
+    String type;
+
+    Pen(String name, String color, String type) {
+        this.name = name;
+        this.color = color;
+        this.type = type;
+    }
+
+    public void print_info() { // Changed return type from String to void
+        System.out.println("Name: " + this.name);
+        System.out.println("Color: " + this.color);
+        System.out.println("Type: " + this.type);
+    }
+}
+
+public class Main { // Changed class name to Main (Java naming convention)
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Pen p = new Pen("Cello", "Blue", "Gel");
+        p.print_info();
     }
 }
