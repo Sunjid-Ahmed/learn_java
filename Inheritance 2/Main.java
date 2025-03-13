066dd75639e199eec2e107a35884ecb439481b9c
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-03-13 15:30:04 +0600

    Add files via upload

---
+++ Inheritance 2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+        engine.model = "V8";
+        engine.year = 2020;
+        engine.horsePower = 500;
+        engine.print_info();
+        engine.print_info();
+    }
+}
