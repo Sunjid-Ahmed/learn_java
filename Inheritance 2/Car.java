41ed54ee1dcc69b9e1f410ae53f9258c1712fd58
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-03-13 16:02:48 +0600

    Delete Inheritance 2 directory

--- Inheritance 2/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    public String model;
-    public int year;
-
-    public void print_info() {
-        System.out.println("Model: " + model);
-        System.out.println("Year: " + year);
-    }
-}
