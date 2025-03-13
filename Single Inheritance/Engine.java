90552e2acb4b692f91d678aef597c1d0ca368dcc
sunjid ahmed <140771553+Sunjid-Ahmed@users.noreply.github.com>
2025-03-13 16:04:25 +0600

    Add files via upload

---
+++ Single Inheritance/Engine.java
@@ -0,0 +1,10 @@
+public class Engine extends Car {
+    public int horsePower;
+
+    public void print_info() {
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Horse Power: " + horsePower);
+    }
+
+}
---
+++ Single Inheritance/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Engine engine = new Engine();
+        engine.model = "V8";
+        engine.year = 2020;
+        engine.horsePower = 500;
+        engine.print_info();
+    }
+}
